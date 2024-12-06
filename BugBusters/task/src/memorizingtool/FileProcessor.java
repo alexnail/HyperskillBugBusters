5ab96c0674c2fb283c1a355164a6fa13640a569d
aleksandrsfridmans <alexnail@gmail.com>
2024-12-06 11:15:23 +0000

    Step 1. Step completed

--- BugBusters/task/src/memorizingtool/FileProcessor.java
+++ BugBusters/task/src/memorizingtool/FileProcessor.java
@@ -11,81 +11,86 @@ import java.util.List;
 //One sunny morning, as Lily strolled by the village square, she noticed a...
 
 /**
- This class provides functionality for processing files in some way.
- The FileProcessor class offers methods to read and write files.
-
-  With the FileProcessor class at our disposal, we can confidently work with files in this application.
- It simplifies the process of reading, writing, and managing files, allowing us to focus on the actual logic of our programs.
+ * This class provides functionality for processing files in some way.
+ * The FileProcessor class offers methods to read and write files.
+ * <p>
+ * With the FileProcessor class at our disposal, we can confidently work with files in this application.
+ * It simplifies the process of reading, writing, and managing files, allowing us to focus on the actual logic of our programs.
  */
 class FileReaderInteger {
 
-  static ArrayList<Integer> list = new ArrayList<>();
+    static ArrayList<Integer> list = new ArrayList<>();
 
-  public ArrayList<Integer> read(String fileName) throws IOException {
+    public ArrayList<Integer> read(String fileName) throws IOException {
+        list.clear();
 
-    list.clear();
-
-    List<String> lines = Files.readAllLines(Paths.get(fileName)  , StandardCharsets.UTF_8 );
-
-    for (String s : lines) {
-
-      list.add( Integer.parseInt(s)  ) ;
-
+        List<String> lines = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+        for (String s : lines) {
+            list.add(Integer.parseInt(s));
+        }
+        return list;
     }
-
-    return list ;
-
-  }
 
 }
 
 //peculiar shimmering object hidden behind a cluster of bushes. Intrigued, she cautiously...
-class  FileWriterInteger   {
-  public   void  write  (  String  fileName,  ArrayList <Integer>  data  ) throws  IOException  {
-    FileWriter  fileWriter  =  new  FileWriter  (  fileName  )  ;
-    PrintWriter  printWriter  =  new  PrintWriter  (  fileWriter  )  ;
-    for  (  Integer  i  :  data  )  {
-      printWriter  .  println  (  i  )  ;
+class FileWriterInteger {
+    public void write(String fileName, ArrayList<Integer> data) throws IOException {
+        FileWriter fileWriter = new FileWriter(fileName);
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+        for (Integer i : data) {
+            printWriter.println(i);
+        }
+        printWriter.close();
     }
-    printWriter  .  close  (  )  ;
-  }
 }
 
-class  FileReaderWords {  private final    ArrayList<String>  list  =  new   ArrayList<>();  public   ArrayList<String>  read(String  fileName)  throws  IOException {  list.clear();  List<String>  lines  =  Files.readAllLines(Paths.get(fileName),  StandardCharsets.UTF_8);  for  (String  s  :  lines)  {  list.add(s);  }  return  list;  }  }
+class FileReaderWords {
+    private final ArrayList<String> list = new ArrayList<>();
+
+    public ArrayList<String> read(String fileName) throws IOException {
+        list.clear();
+        List<String> lines = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+        for (String s : lines) {
+            list.add(s);
+        }
+        return list;
+    }
+}
 
 class FileWriterWords {
-  public    void    write   (  String  fileName,    ArrayList < String >  data  )   throws    IOException  {
-    FileWriter  fileWriter  =  new    FileWriter  (  fileName   )  ;
-    PrintWriter    printWriter  =  new    PrintWriter  (  fileWriter  )  ;
-    for    (  String  i  :  data   )  {
-      printWriter   .  println  (  i  )  ;
+    public void write(String fileName, ArrayList<String> data) throws IOException {
+        FileWriter fileWriter = new FileWriter(fileName);
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+        for (String i : data) {
+            printWriter.println(i);
+        }
+        printWriter.close();
     }
-    printWriter   .  close  (  )  ;
-  }
 }
 
 //approached the bushes and discovered a beautifully crafted silver key. The key...
-class   FileReaderBoolean   {
-  private  final  ArrayList<Boolean>   list   =   new   ArrayList<>();
+class FileReaderBoolean {
+    private final ArrayList<Boolean> list = new ArrayList<>();
 
-  public  ArrayList<Boolean>  read   (  String  fileName  )  throws   IOException  {
-    list  .  clear  (  )  ;
-    List<String>  lines  =  Files  .  readAllLines  (  Paths  .  get  (  fileName  )  ,  StandardCharsets  .  UTF_8  )  ;
-    for  (  String  s  :  lines  )  {
-      list  .  add  (  Boolean  .  parseBoolean  (  s  )  )  ;
+    public ArrayList<Boolean> read(String fileName) throws IOException {
+        list.clear();
+        List<String> lines = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+        for (String s : lines) {
+            list.add(Boolean.parseBoolean(s));
+        }
+        return list;
     }
-    return  list  ;
-  }
 }
 
 //gleamed in the sunlight, as if beckoning her to uncover its secrets.
-class  FileWriterBoolean    {
-  public   void  write  (  String  fileName,  ArrayList<Boolean>  data  )   throws  IOException   {
-    FileWriter  fileWriter  =  new   FileWriter  (  fileName  ) ;
-    PrintWriter   printWriter   =  new  PrintWriter  (  fileWriter  ) ;
-    for  (  Boolean  i  :  data  )   {
-      printWriter . println  (  i  ) ;
+class FileWriterBoolean {
+    public void write(String fileName, ArrayList<Boolean> data) throws IOException {
+        FileWriter fileWriter = new FileWriter(fileName);
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+        for (Boolean i : data) {
+            printWriter.println(i);
+        }
+        printWriter.close();
     }
-    printWriter  .  close  (  )  ;
-  }
 }
